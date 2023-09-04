339a4521b204ea0eb8fd5f7866c47ace5c36d269
Keith Stanley <iamstan13y@gmail.com>
2023-09-04 16:37:34 +0200

    Generate & Save Code.

---
+++ UrlShortener.API/Entities/ShortenedUrl.cs
@@ -0,0 +1,10 @@
+﻿namespace UrlShortener.API.Entities;
+
+public class ShortenedUrl
+{
+    public Guid Id { get; set; }
+    public string? LongUrl { get; set; } = string.Empty;
+    public string? ShortUrl { get; set; } = string.Empty;
+    public string? Code { get; set; } = string.Empty;
+    public DateTime CreatedOnUtc { get; set; }
+}
