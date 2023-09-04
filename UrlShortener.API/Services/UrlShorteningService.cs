ed43806e1d0abe378ac2c9d79a8e27500c40ffd3
Keith Stanley <iamstan13y@gmail.com>
2023-09-04 16:37:41 +0200

    Generate & Save Code.

---
+++ UrlShortener.API/Services/UrlShorteningService.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore;
+using UrlShortener.API.Entities;
+
+namespace UrlShortener.API.Services;
+
+public class UrlShorteningService
+{
+    public const int NumberOfCharsInShortLink = 7;
+    private const string Alphabet = "QWERTYUIOPASDFGHJKLZXCVBNMqwertyuiopasdfghjklzxcvbnm1234567890";
+    private readonly AppDbContext _context;
+
+    private readonly Random _random = new();
+
+    public UrlShorteningService(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<string> GenerateUniqueCode()
+    {
+        var codeChars = new char[NumberOfCharsInShortLink];
+
+        while (true)
+        {
+            for (int i = 0; i < NumberOfCharsInShortLink; i++)
+            {
+                var randomIndex = _random.Next(Alphabet.Length - 1);
+                codeChars[i] = Alphabet[randomIndex];
+            }
+
+            var code = new string(codeChars);
+
+            if (!await _context.ShortenedUrls!.AnyAsync(s => s.Code == code))
+            {
+                return code;
+            }
+        }
+    }
+}
