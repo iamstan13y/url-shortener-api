35bb957756c1bb61f043e8f7454118db7227f772
Keith Stanley <iamstan13y@gmail.com>
2023-09-04 16:36:41 +0200

    Generate & Save Code.

---
+++ UrlShortener.API/Entities/AppDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using UrlShortener.API.Services;
+
+namespace UrlShortener.API.Entities;
+
+public class AppDbContext : DbContext
+{
+    protected AppDbContext(DbContextOptions options) 
+        : base(options)
+    {
+    }
+
+    public DbSet<ShortenedUrl>? ShortenedUrls { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<ShortenedUrl>(builder =>
+        {
+            builder.Property(s => s.Code).HasMaxLength(UrlShorteningService.NumberOfCharsInShortLink);
+            builder.HasIndex(s => s.Code).IsUnique();
+        });
+    }
+}
