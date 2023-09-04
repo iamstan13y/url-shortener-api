0fa4132d4badf2b693614fbbfbf3a21374d6f2df
Keith Stanley <iamstan13y@gmail.com>
2023-09-04 16:37:37 +0200

    Generate & Save Code.

---
+++ UrlShortener.API/Extensions/MigrationExtensions.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using UrlShortener.API.Entities;
+
+namespace UrlShortener.API.Extensions;
+
+public static class MigrationExtensions
+{
+    public static void ApplyMigrations(this WebApplication app)
+    {
+        using var scope = app.Services.CreateScope();
+
+        var context = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+
+        context.Database.Migrate();
+    }
+}
